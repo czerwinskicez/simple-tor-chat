8210b4450872f832c35e6450ae0a0383886a6860
Cezary Czerwiński <czerwinskicez@gmail.com>
2025-08-19 18:43:34 +0200

    update of update.sh

--- update.sh
+++ update.sh
@@ -5,13 +5,17 @@ set -e
 cd /home/nodeapps/very-simple-chat || { echo "Error: Cannot find application directory."; exit 1; }
 
 echo "Downloading."
-git pull
+git fetch --all
+git reset --hard origin/master
 
 echo "Updating dependencies."
 npm install
 
 echo "Reloading application."
 pm2 reload server
+
+echo "Cleaning up."
+git clean -fd
 
 echo ""
 echo "Update completed successfully!"
